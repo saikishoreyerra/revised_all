ae4e5502695a302df66fd95d12e71a1e5ce8b8da
Sai Kishore Neelakantam Yerra <darling_ysk@Sais-MacBook-Pro.local>
2024-04-16 22:25:11 +0530

    todo list widget added

---
+++ lib/todo_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TodoListWidget extends StatelessWidget {
+  const TodoListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
