df9e03e99b64be67e61aaabf711057bbde1196d6
Sai Kishore Neelakantam Yerra <darling_ysk@Sais-MacBook-Pro.local>
2024-04-16 22:27:29 +0530

    modified todolist ui

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TodoListWidget extends StatelessWidget {
   const TodoListWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: Container(color: Colors.red,),);
   }
 }
